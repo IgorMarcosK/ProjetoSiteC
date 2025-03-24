e9abdd8a475f5e10c44aecd6670e77e3a8bdbe4d
IgorMarcosK <112531270+IgorMarcosK@users.noreply.github.com>
2025-03-24 00:35:07 -0300

    start

---
+++ SalesWebMvc21/Data/SalesWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc21.Models;
+
+namespace SalesWebMvc21.Data
+{
+    public class SalesWebMvcContext : DbContext
+    {
+        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvc21.Models.Department> Department { get; set; } = default;
+    }
+}
